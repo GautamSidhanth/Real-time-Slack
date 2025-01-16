5a6d526dac5cdc7c1998eaa4b718b7db2d6738ff
Gautam Sidhanth <gautamsidhanth14@gmail.com>
2025-01-16 16:34:30 +0530

    first commit

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,7 +1,7 @@
 import GitHub from "@auth/core/providers/github";
 import { convexAuth } from "@convex-dev/auth/server";
 import Google from "@auth/core/providers/google";
-import { Password } from "@convex-dev/auth/providers/password";
+import { Password } from "@convex-dev/auth/providers/Password";
 import { DataModel } from "./_generated/dataModel";
 
 const CustomPassword = Password<DataModel>({
